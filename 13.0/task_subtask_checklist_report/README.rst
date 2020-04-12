c065bd1ad9d7e73bbe660a1d57eb83a6246fe70c
Alpha Omega <28748254+xAlphaOmega@users.noreply.github.com>
2020-04-11 20:42:48 -0400

    :heavy_plus_sign: [ADD] TASK LIST

---
+++ 13.0/task_subtask_checklist_report/README.rst
@@ -0,0 +1,12 @@
+
+=====================================
+Report Project Task SubTask Checklist
+=====================================
+If you like Project Task SubTask Checklist application and you want to print checklist report then you can download this module in just 4.99 Euro. You can print checklist report with chatter(log)/without chatter for specific Task/Project
+
+Key features:
+-------------
+* Print task wise checklist report.
+* Print task wise checklist report with chatter option.
+* Print project wise checklist report.
+* Print project wise checklist report with chatter option.
---
+++ 13.0/task_subtask_checklist_report/__manifest__.py
@@ -0,0 +1,38 @@
+{
+    'name': 'Report Project Task SubTask Checklist',
+    'version': '1.0.0',
+    'author': 'FreelancerApps',
+    'category': 'Project',
+    'summary': 'You can print checklist report with chatter(log)/without chatter for specific Task/Project Project Task SubTask Checklist Project Task SubTask Project Task Checklist Project SubTask Task Project SubTask Checklist Task Project SubTask Project Checklist Task Project Checklist SubTask Task Project Checklist Task SubTask Project SubTask Task Checklist SubTask Project Task Project Checklist',
+    'depends': ['base', 'web', 'project', 'task_subtask_checklist'],
+    'description': '''
+Added CheckList And CheckList Related Features Into Project
+-----------------------------------------------------------
+If you like Project Task SubTask Checklist application and you want to print checklist report then you can download this module in just 4.99 Euro. You can print checklist report with chatter(log)/without chatter for specific Task/Project
+
+Key features:
+-------------
+* Print task wise checklist report.
+* Print task wise checklist report with chatter option.
+* Print project wise checklist report.
+* Print project wise checklist report with chatter option.
+
+<Search Keyword for internal user only>
+---------------------------------------
+Project Task SubTask Checklist Project Task SubTask Project Task Checklist Project SubTask Task Project SubTask Checklist Task Project SubTask Project Checklist Task Project Checklist SubTask Task Project Checklist Task SubTask Project SubTask Task Checklist SubTask Project Task Project Checklist
+    ''',
+    'data': [
+        'views/task_checklist_report.xml',
+        'views/task_checklist_chatter_report.xml',
+        'views/project_checklist_report.xml',
+        'views/project_checklist_chatter_report.xml',
+        'views/checklist_reports.xml',
+    ],
+    'images': ['static/description/task_subtask_checklist_report_banner.png'],
+    'price': 4.99,
+    'currency': 'USD',
+    'license': 'OPL-1',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
